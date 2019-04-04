2470057ca2b4f1922be9e5333fda867175ae8dde
lcglcglcglcglcg <1421061623@qq.com>
2019-04-04 19:56:46 +0800

    search finish

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="city">
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
     <city-list :letter="letter" :hotCities='hotCities' :cities="cities"></city-list>
     <city-alphabet :cities="cities" @change='handleLetterClick'></city-alphabet>
   </div>
--- src/components/city/components/Search.vue
+++ src/components/city/components/Search.vue
@@ -1,27 +1,103 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="请输入城市名/拼音">
+  <div>
+    <div class="search">
+      <input class="search-input" type="text" placeholder="请输入城市名/拼音" v-model="keyword">
+    </div>
+    <div v-show="keyword" class="search-content" ref="search">
+      <ul>
+        <li class="search-item border-bottom" v-for="item in list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有匹配的数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name:'CitySearch'
+import Bscroll from "better-scroll";
+import { clearTimeout, setTimeout } from "timers";
+export default {
+  name: "CitySearch",
+  props: {
+    cities: Object
+  },
+  data() {
+    return {
+      keyword: "",
+      list: [],
+      timer: null
+    };
+  },
+  computed: {
+    hasNoData() {
+      return !this.list.length;
+    }
+  },
+  mounted() {
+    this.scroll = new Bscroll(this.$refs.search);
+  },
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      if (!this.keyword) {
+        this.list = [];
+        return;
+      }
+      this.timer = setTimeout(() => {
+        const result = [];
+        for (let i in this.cities) {
+          this.cities[i].forEach(val => {
+            if (
+              val.spell.indexOf(this.keyword) > -1 ||
+              val.name.indexOf(this.keyword) > -1
+            ) {
+              result.push(val);
+            }
+          });
+        }
+        this.list = result;
+        console.log(result);
+      }, 300);
+    }
   }
+};
 </script>
 
 <style lang="stylus" scoped >
-@import '~styles/varibles.styl'
-  .search
-    height .72rem
-    padding .1rem
-    background-color $bgColor
-    .search-input
-      box-sizing border-box
-      width 100%
-      height .62rem
-      line-height .62rem
-      text-align center
-      border-radius .06rem
-      padding 0 .1rem
+@import '~styles/varibles.styl';
+
+.search {
+  height: 0.72rem;
+  padding: 0.1rem;
+  background-color: $bgColor;
+
+  .search-input {
+    box-sizing: border-box;
+    width: 100%;
+    height: 0.62rem;
+    line-height: 0.62rem;
+    text-align: center;
+    border-radius: 0.06rem;
+    padding: 0 0.1rem;
+  }
+}
+
+.search-content {
+  z-index: 1;
+  overflow: hidden;
+  position: absolute;
+  top: 1.75rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #eee;
+
+  .search-item {
+    line-height: 0.62rem;
+    padding-left: 0.2rem;
+    color: #666;
+    background: #fff;
+  }
+}
 </style>
