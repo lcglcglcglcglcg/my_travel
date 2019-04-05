48a546f2d7399e33f85273b0b37cb217b620ceba
lcglcglcglcglcg <1421061623@qq.com>
2019-04-05 19:12:35 +0800

    header animation

--- src/components/detail/components/Banner.vue
+++ src/components/detail/components/Banner.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <div class="banner" @click="handleBannerClick">
-      <img class="banner-img" :src="imgs[0].imgUrl">
-      <div class="banner-info">
-        <div class="banner-title">大连圣亚海洋世界(AAAA景区)</div>
-        <div class="banner-number">
-          <span class="iconfont banner-icon">&#xe643;</span>
-          39
+    <div class="banner" ref="banner">
+      <div class="banner-wrapper" @click="handleBannerClick">
+        <div class="banner-img">
+          <img class="banner-img-box" :src="imgs[0].imgUrl">
+        </div>
+        <div class="banner-info">
+          <div class="banner-title">大连圣亚海洋世界(AAAA景区)</div>
+          <div class="banner-number">
+            <span class="iconfont banner-icon">&#xe643;</span>
+            39
+          </div>
         </div>
       </div>
     </div>
@@ -16,73 +20,68 @@
 
 <script>
 import CommonGallary from "common/gallary/Gallary";
+import Bscroll from "better-scroll";
 export default {
   name: "DetailBanner",
   data() {
     return {
       showGallary: false,
-      imgs:[
-        {imgUrl:'https://yantuz.cn/mmPic/'},
-        {imgUrl:'https://yantuz.cn/mmPic/?333'},
-        {imgUrl:'https://yantuz.cn/mmPic/?33'},
-        {imgUrl:'https://yantuz.cn/mmPic/?3'}
+      imgs: [
+        { imgUrl: "https://yantuz.cn/mmPic/" },
+        { imgUrl: "https://yantuz.cn/mmPic/?333" },
+        { imgUrl: "https://yantuz.cn/mmPic/?33" },
+        { imgUrl: "https://yantuz.cn/mmPic/?3" },
+        { imgUrl: "https://yantuz.cn/mmPic/?4" },
+        { imgUrl: "https://yantuz.cn/mmPic/?5" }
       ]
-    }
+    };
   },
   components: {
     CommonGallary
   },
+  mounted() {
+    this.scroll = new Bscroll(this.$refs.banner);
+  },
   methods: {
     handleBannerClick() {
-      this.showGallary = true
+      this.showGallary = true;
     },
-    handleGallaryClick(){
-      this.showGallary = false
+    handleGallaryClick() {
+      this.showGallary = false;
     }
   }
 };
 </script>
 
 <style lang="stylus" scoped >
-.banner {
-  position: relative;
-  overflow: hidden;
-  height: 0;
-  padding-bottom: 55%;
-
-  .banner-img {
-    width: 100%;
-  }
-
-  .banner-info {
-    display: flex;
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    line-height: 0.6rem;
-    color: #fff;
-    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
-
-    .banner-title {
-      flex: 1;
-      font-size: 0.32rem;
-      padding: 0 0.2rem;
-    }
-
-    .banner-number {
-      padding: 0 0.4rem;
-      line-height: 0.32rem;
-      height: 0.32rem;
-      margin-top: 0.14rem;
-      border-radius: 0.2rem;
-      background: rgba(0, 0, 0, 0.8);
-      font-size: 0.24rem;
-
-      .banner-icon {
+  .banner-wrapper
+    position: relative;
+    .banner-img
+      overflow hidden
+      height: 0;
+      padding-bottom: 55%;
+      .banner-img-box
+        width: 100%;
+    .banner-info
+      display: flex;
+      position: absolute;
+      left: 0;
+      right: 0;
+      line-height: 0.6rem;
+      color: #fff;
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
+      .banner-title
+        flex: 1;
+        font-size: 0.32rem;
+        padding: 0 0.2rem;
+      .banner-number
+        padding: 0 0.4rem;
+        line-height: 0.32rem;
+        height: 0.32rem;
+        margin-top: 0.14rem;
+        border-radius: 0.2rem;
+        background: rgba(0, 0, 0, 0.8);
         font-size: 0.24rem;
-      }
-    }
-  }
-}
+        .banner-icon
+          font-size: 0.24rem;
 </style>
---
+++ src/components/detail/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="detail-header">
+    <div @click="back" v-show="showAbs" class="header-abs">
+      <span class="iconfont header-abs-back">&#xe78a;</span>
+    </div>
+    <div v-show="!showAbs" class="header-fixed" :style="opacityStyle">
+      景点选择
+      <router-link to='/home'>
+        <span class="inconfont header-fixed-back">&#xe78a;</span>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'DetailHeader',
+    data(){
+      return {
+        showAbs: true,
+        opacityStyle:{
+          opacity: 0
+        }
+      }
+    },
+    activated(){
+      window.addEventListener('scroll',this.handleScroll)
+    },
+    methods:{
+      back() {
+        this.$router.go(-1);
+      },
+      handleScroll(){
+        let top = document.documentElement.scrollTop
+        if(top > 50){
+          let opacity = top / 140
+          opacity = opacity > 1 ? 1 : opacity
+          this.opacityStyle = { opacity }
+          this.showAbs = false
+        } else{
+          this.showAbs = true
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped >
+  @import '~styles/varibles.styl'
+  .header-abs
+    position: absolute;
+    left: 0.2rem;
+    top: 0.2rem;
+    width: 0.8rem;
+    height: 0.8rem;
+    border-radius: 0.4rem;
+    background: rgba(0, 0, 0, 0.5);
+    text-align: center;
+    line-height: 0.8rem;
+    .header-abs-back
+      font-size: 0.5rem;
+      color: #fff;
+  .header-fixed
+    position fixed
+    top 0
+    left 0
+    right 0
+    height .86rem
+    line-height .86rem
+    text-align center
+    color #fff
+    background $bgColor
+    font-size .32rem
+    .header-fixed-back
+      position absolute
+      left 0
+      top 0
+      width .64rem
+      font-size .4rem
+      color #fff
+</style>
